6b6528b4ce6f9f3c650d211010671693f0d0ccb7
ferchuqui <fernando.farji@gmail.com>
2025-07-30 13:49:02 -0300

    clase5

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,19 @@
 import './TodoItem.css';
-function TodoItem(props){
+
+function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className="Icon Icon-check Icon-check--active">🍎</span>
-      <p className="TodoItem-p TodoItem-p--complete">{props.text}</p>
-      <span className="Icon Icon-delete">❌</span>
+      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+        V
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+      <span className="Icon Icon-delete">
+        X
+      </span>
     </li>
-  )
+  );
 }
+
 export { TodoItem };
